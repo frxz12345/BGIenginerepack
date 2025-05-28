469ec08b392f863226533a1762321ef41bcae15c
frxz12345 <156357924+frxz12345@users.noreply.github.com>
2025-05-28 18:56:06 +0800

    Update BGI_V2_pack.py

--- BGI_V2_pack.py
+++ BGI_V2_pack.py
@@ -1,9 +1,8 @@
 import struct, os
-
-cn = './CN\\'
+Game = '黄昏のフォルクローレ'
+cn = './14b\\'
 if not os.path.exists(cn):
     os.mkdir(cn)
-key = key.encode('CP932')
 files = os.listdir(cn)
 filecont = len(files)
 f = open('data01110.arc', 'wb')
@@ -11,16 +10,15 @@ f.write(b'BURIKO ARC20')
 f.write(struct.pack('i', filecont))
 pos = 0
 data = b''
+fileheader = b'DSC FORMAT 1.00\x00'
+fileheader=b''
 for file in files:
     size = os.stat(cn + file).st_size
-    nl = len(file.encode('CP932'))
-    null = (96 - nl) * b'\x00'
-    data = data + file.encode('cp932')
-    data = data + null
+    data = data + struct.pack(f'{96}s', file.encode('cp932'))
     data = data + struct.pack('i', pos)
     data = data + struct.pack('i', size)
-    data = data + struct.pack('i', 0)*6
-    pos = pos + size + 16
+    data = data + struct.pack(f'{24}s', ''.encode('cp932'))
+    pos = pos + size + len(fileheader)
 pos = 0
 for b in data:
     b = b#(b - key[pos % len(key)]) & 0xFF
@@ -31,7 +29,6 @@ for file in files:
     f1 = open(cn + file, 'rb')
     b = f1.read()
     f1.close()
-    f.write(b'DSC FORMAT 1.00')
-    f.write(b'\x00')
+    f.write(fileheader)
     f.write(b)
 f.close()
